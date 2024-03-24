3d7979299ef06ed8a43753326051ce335be4a2f7
KatarzynaBanach <katarzyna.banach98@gmail.com>
2024-03-24 12:52:54 +0100

    beam filter & map

--- Beam/beam_pipeline.py
+++ Beam/beam_pipeline.py
@@ -1,5 +1,12 @@
 import apache_beam as beam
 
+def is_empty(row):
+    cols = row.split(';')
+    if cols[0] == '' or cols[2] == '' or cols[3] == '' or cols[4] == '' :
+        return False
+    else:
+        return True
+
 def name_capitalize(row):
     cols = row.split(';')
     cols[0] = cols[0].capitalize()
@@ -8,16 +15,34 @@ def name_capitalize(row):
 def surname_cleaning(row):
     import re
     cols = row.split(';')
-    cols[2] = 
+    row = []
+    for idx, col in enumerate(cols):
+        #exeption for date column
+        if idx == 3:
+            row.append(col)
+        else:
+            row.append(re.sub(r'[#$%&.?]','',col))
+
+    return ';'.join(row)
+
+def date_unify(row):
+    import pandas as pd
+    cols = row.split(';')
+    cols[3] = str(pd.to_datetime(cols[3], dayfirst=True).date())
+    return ';'.join(cols)
+
 
 p = beam.Pipeline()
 
 (p
  | beam.io.ReadFromText("names_data.csv", skip_header_lines=True)
+ | beam.Filter(is_empty)
  | beam.Map(name_capitalize)
+ | beam.Map(surname_cleaning)
+ | beam.Map(date_unify)
  | beam.Map(print)
 )
 
 p.run()
 
-#first install apache_beam !pip install apache_beam
+#first install apache_beam !pip install apache_beam & pandas
