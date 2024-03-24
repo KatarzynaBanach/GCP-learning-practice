5e9a02aa292f6b41f352a75bae92b30e0e7aaca2
KatarzynaBanach <katarzyna.banach98@gmail.com>
2024-03-24 13:31:09 +0100

    total counts

--- Beam/beam_pipeline.py
+++ Beam/beam_pipeline.py
@@ -2,7 +2,7 @@ import apache_beam as beam
 
 def is_empty(row):
     cols = row.split(';')
-    if cols[0] == '' or cols[2] == '' or cols[3] == '' or cols[4] == '' :
+    if cols[0] == '' or cols[2] == '' or cols[3] == '' or cols[4] == '' or cols[6] == '' :
         return False
     else:
         return True
@@ -12,7 +12,7 @@ def name_capitalize(row):
     cols[0] = cols[0].capitalize()
     return ';'.join(cols)
 
-def surname_cleaning(row):
+def symbols_cleaning(row):
     import re
     cols = row.split(';')
     row = []
@@ -25,24 +25,64 @@ def surname_cleaning(row):
 
     return ';'.join(row)
 
+def join_names(row):
+    cols = row.split(';')
+    names = ' '.join([c for c in cols[0:3] if c != ''])
+    rest = ';'.join(cols[3:])
+    return names + ';' + rest
+
 def date_unify(row):
     import pandas as pd
     cols = row.split(';')
-    cols[3] = str(pd.to_datetime(cols[3], dayfirst=True).date())
+    cols[1] = str(pd.to_datetime(cols[1], dayfirst=True).date())
     return ';'.join(cols)
 
 
 p = beam.Pipeline()
 
-(p
- | beam.io.ReadFromText("names_data.csv", skip_header_lines=True)
- | beam.Filter(is_empty)
- | beam.Map(name_capitalize)
- | beam.Map(surname_cleaning)
- | beam.Map(date_unify)
- | beam.Map(print)
+cleaned_data = (
+    p
+    | beam.io.ReadFromText("names_data.csv", skip_header_lines=True)
+    | beam.Filter(is_empty)
+    | beam.Map(name_capitalize)
+    | beam.Map(symbols_cleaning)
+    | beam.Map(join_names)
+    | beam.Map(date_unify)
+)
+
+pinterest_data = (
+    cleaned_data
+    | beam.Filter(lambda row: row.split(';')[4].lower() == 'pinterest')
+)
+
+facebook_data = (
+    cleaned_data
+    | beam.Filter(lambda row: row.split(';')[4].lower() == 'facebook')
+)
+
+(
+    cleaned_data
+    | 'total count' >> beam.combiners.Count.Globally()
+    | 'total map' >> beam.Map(lambda x: 'Total Count: '+ str(x))
+    | 'total print' >> beam.Map(print)
+)
+
+(
+    pinterest_data
+    | 'pinterest count' >> beam.combiners.Count.Globally()
+    | 'pinterest map' >> beam.Map(lambda x: 'Pinterest count: ' + str(x))
+    | 'pinterest print' >> beam.Map(print)
+)
+
+(
+    facebook_data
+    | 'facebook count' >> beam.combiners.Count.Globally()
+    | 'facebook map' >> beam.Map(lambda x: 'Facebook count: ' + str(x))
+    | 'facebook print' >> beam.Map(print)
 )
 
 p.run()
 
 #first install apache_beam !pip install apache_beam & pandas
+
+#ideas: country mapping from different file
