7b9b7c7567145cfd3c31ce62f4dea6178941d2a6
KatarzynaBanach <102869680+KatarzynaBanach@users.noreply.github.com>
2024-04-08 19:51:53 +0200

    Update pipeline_in_func.py

--- Beam_templates/pipeline_in_func.py
+++ Beam_templates/pipeline_in_func.py
@@ -1,7 +1,6 @@
 # using funtion to transform pipeline
 import apache_beam as beam
 
-# function
 def number_add(pcall,el):
   return (
       pcall
@@ -10,12 +9,30 @@ def number_add(pcall,el):
   
 p = beam.Pipeline()
 
-
 number = p | beam.Create([1,2,3,4,5])
+final_p = number_add(number,1)  # passing pipeline as a function
 
-# passing pipeline as a function
-final_p = number_add(number,1)
-
-# print pcollection
 final_p | beam.Map(print)
+p.run()
+
+
+
+# --------------------------------------------------------------------------------------------
+# it is nice option to make ptransformations resable
+
+def number_add(pcall,el):
+  return (
+      pcall
+      | f'add {el}' >> beam.Map(lambda x: x+el)
+  )
+
+p = beam.Pipeline()
+numbers = p | beam.Create([1,2,3,4,5])
+
+final_p = number_add(numbers,1)
+final_p | 'print_1' >> beam.Map(print)
+
+final_2 = number_add(numbers,10)
+final_2 | 'print_10' >> beam.Map(print)
+
 p.run()
