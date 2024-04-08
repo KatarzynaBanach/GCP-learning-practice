c8876b01895bca8500a209ae91db4dfb0c5774d6
KatarzynaBanach <102869680+KatarzynaBanach@users.noreply.github.com>
2024-04-08 20:05:01 +0200

    Create side_input.py

---
+++ Beam_templates/side_input.py
@@ -0,0 +1,18 @@
+import apache_beam as beam
+
+def multiply_by_side_input(element, side_input):
+    side_value = side_input['multiplier']
+    return element * side_value
+
+with beam.Pipeline() as p:
+    # Main input PCollection
+    main_input = p | beam.Create([1, 2, 3, 4, 5])
+
+    # Side input PCollection
+    side_input = p | "Create side input" >> beam.Create([{'multiplier': 10}])
+
+    # Apply beam.Map with side input
+    multiplied_values = main_input | beam.Map(multiply_by_side_input, side_input=beam.pvalue.AsSingleton(side_input))
+
+    # Output
+    multiplied_values | beam.Map(print)
