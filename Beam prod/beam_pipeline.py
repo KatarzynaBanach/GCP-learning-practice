64965d0c9f9436f73b153954c90720e9c2d2bacc
KatarzynaBanach <102869680+KatarzynaBanach@users.noreply.github.com>
2024-04-09 21:58:17 +0200

    Update beam_pipeline.py

--- Beam prod/beam_pipeline.py
+++ Beam prod/beam_pipeline.py
@@ -4,6 +4,11 @@ import google.cloud.storage as gcs
 from apache_beam.runners.runner import PipelineState
 import argparse
 from apache_beam.options.pipeline_options import PipelineOptions
+import google.cloud.storage as gcs
+import os
+
+gcs_bucket = 'temp_beam_location_1'
+schema_definition = 'customer:STRING, birthday:DATE, nationality:STRING, sex:STRING, _created_at:DATETIME'
 
 def get_arguments():
   parser = argparse.ArgumentParser()
@@ -103,109 +108,107 @@ def row_count_write(pcol, source):
             | f'{source} to_cs' >> beam.io.textio.WriteToText(output_results,file_name_suffix='.txt')
             )
 
-# Create dataset if needed.
-client = bigquery.Client()
-dataset_name = 'registrated_clients'
-dataset_id = f'{client.project}.{dataset_name}'
+def run():
 
-try:
-    client.get_dataset(dataset_id)
-except:
-    dataset = bigquery.Dataset(dataset_id)
-    dataset.location = "EU"
-    dataset.description = "Registrated client details"
-    dataset_ref = client.create_dataset(dataset, timeout=60)
-def is_empty(row):
-    cols = row.split(';')
-    if cols[0] == '' or cols[2] == '' or cols[3] == '' or cols[4] == '' or cols[6] == '' :
-        return False
-    else:
-        return True
+  # Create dataset if needed.
+  client = bigquery.Client()
+  dataset_name = 'registrated_clients'
+  dataset_id = f'{client.project}.{dataset_name}'
 
+  # Get names for tables based on current project.
+  pinterest_table_name = f'{client.project}:{dataset_name}.pinterest_data'
+  facebook_table_name = f'{client.project}:{dataset_name}.facebook_data'
+  
+  try:
+      client.get_dataset(dataset_id)
+  except:
+      dataset = bigquery.Dataset(dataset_id)
+      dataset.location = "EU"
+      dataset.description = "Registrated client details"
+      dataset_ref = client.create_dataset(dataset, timeout=60)
+  def is_empty(row):
+      cols = row.split(';')
+      if cols[0] == '' or cols[2] == '' or cols[3] == '' or cols[4] == '' or cols[6] == '' :
+          return False
+      else:
+          return True
+  
+  # Clear bucket with results.
+  bucket = gcs.Client().get_bucket(gcs_bucket)
+  for blob in bucket.list_blobs(prefix='easy_beam_test/'):
+    blob.delete()
+  
+  # argv = [
+  # '--project={0}'.format(client.project),
+  # '--job_name=myjob',
+  # '--save_main_session',
+  # '--worker_machine_type=e2-standard-2',
+  # '--staging_location=gs://{0}/staging/'.format("gs://temp_beam_location/staging"),
+  # '--temp_location=gs://{0}/staging/'.format("gs://temp_beam_location/temp"),
+  # '--runner={0}'.format("DataflowRunner"),
+  # '--region={0}'.format("us-central1"),
+  # '--max_num_workers=5'
+  # ]
+  # p = beam.Pipeline(argv=argv)
+  
+  
+  # Get and pass options for Pipeline.
+  args = get_arguments()
+  options = PipelineOptions(
+      project=args['project'],
+      region=args['region'],  # Choose the appropriate region
+      job_name='examplejob4',
+      temp_location='gs://temp_beam_location_1/staging', 
+      staging_location='gs://temp_beam_location_1/staging',
+      runner=args['runner'],
+      worker_machine_type='e2-standard-2'
+  )
+  
+  p = beam.Pipeline(options=options)
+  
+  # Reading data from Cloud Storage and cleaning it.
+  cleaned_data = (
+      p
+      | beam.io.ReadFromText("gs://temp_beam_location_1/customer/customer_data.csv", skip_header_lines=True)
+      | beam.Filter(is_empty)
+      | beam.Distinct()
+      | beam.Map(name_capitalize)
+      | beam.Map(symbols_cleaning)
+      | beam.Map(join_names)
+      | beam.Map(date_unify)
+      | beam.Map(add_created_at_and_id)
+  )
+  
+  # Splitting pcollections by source.
+  pinterest_data = split_by_sources(cleaned_data, 'pinterest')
+  facebook_data = split_by_sources(cleaned_data, 'facebook')
+  
+  # Counting rows and writing the information into Cloud Storage.
+  row_count_write(cleaned_data, 'Total')
+  row_count_write(pinterest_data, 'Pinterest')
+  row_count_write(facebook_data, 'Facebook')
+  
+  # Writing splitted data into BigQuery.
+  write_to_bq(pinterest_data, 'pinterest', pinterest_table_name)
+  write_to_bq(facebook_data, 'facebook', facebook_table_name)
+  
+  # Runing pipeline.
+  pipeline_state = p.run().wait_until_finish()
+  
+  # Getting state of pipeline and writing it into Cloud Storage.
+  pipeline_result = ''
+  if pipeline_state == PipelineState.DONE:
+      pipeline_result = 'Pipeline has completed successfully.'
+  elif pipeline_state == PipelineState.FAILED:
+      pipeline_result = 'Pipeline has failed.'
+  else:
+      pipeline_result = 'Pipeline in unknown state.'
+  
+  with open('pipeline_status.txt', 'w') as f:
+      f.write(pipeline_result)
+  bucket.blob('easy_beam_test/pipeline_status.txt').upload_from_filename('pipeline_status.txt')
+  if os.path.exists("pipeline_status.txt"):
+    os.remove("pipeline_status.txt")
 
-
-pinterest_table_name = f'{client.project}:{dataset_name}.pinterest_data'
-facebook_table_name = f'{client.project}:{dataset_name}.facebook_data'
-
-gcs_bucket = 'temp_beam_location_1'
-bucket = gcs.Client().get_bucket(gcs_bucket)
-
-# Clear bucket with results.
-for blob in bucket.list_blobs(prefix='easy_beam_test/'):
-  blob.delete()
-
-# argv = [
-# '--project={0}'.format(client.project),
-# '--job_name=myjob',
-# '--save_main_session',
-# '--worker_machine_type=e2-standard-2',
-# '--staging_location=gs://{0}/staging/'.format("gs://temp_beam_location/staging"),
-# '--temp_location=gs://{0}/staging/'.format("gs://temp_beam_location/temp"),
-# '--runner={0}'.format("DataflowRunner"),
-# '--region={0}'.format("us-central1"),
-# '--max_num_workers=5'
-# ]
-
-# p = beam.Pipeline(argv=argv)
-
-args = get_arguments()
-
-options = PipelineOptions(
-    project=args['project'],
-    region=args['region'],  # Choose the appropriate region
-    job_name='examplejob4',
-    temp_location='gs://temp_beam_location_1/staging', 
-    staging_location='gs://temp_beam_location_1/staging',
-    runner=args['runner'],
-    worker_machine_type='e2-standard-2'
-)
-
-p = beam.Pipeline(options=options)
-
-cleaned_data = (
-    p
-    | beam.io.ReadFromText("gs://temp_beam_location_1/customer/customer_data.csv", skip_header_lines=True)
-    | beam.Filter(is_empty)
-    | beam.Distinct()
-    | beam.Map(name_capitalize)
-    | beam.Map(symbols_cleaning)
-    | beam.Map(join_names)
-    | beam.Map(date_unify)
-    | beam.Map(add_created_at_and_id)
-)
-
-schema_definition = 'customer:STRING, birthday:DATE, nationality:STRING, sex:STRING, _created_at:DATETIME'
-
-# Splitting pcollections by source.
-pinterest_data = split_by_sources(cleaned_data, 'pinterest')
-facebook_data = split_by_sources(cleaned_data, 'facebook')
-
-# Counting rows and writing the information into Cloud Storage.
-row_count_write(cleaned_data, 'Total')
-row_count_write(pinterest_data, 'Pinterest')
-row_count_write(facebook_data, 'Facebook')
-
-# Writing splitted data into BigQuery.
-write_to_bq(pinterest_data, 'pinterest', pinterest_table_name)
-write_to_bq(facebook_data, 'facebook', facebook_table_name)
-
-pipeline_state = p.run().wait_until_finish()
-
-pipeline_result = ''
-if pipeline_state == PipelineState.DONE:
-    pipeline_result = 'Pipeline has completed successfully.'
-elif pipeline_state == PipelineState.FAILED:
-    pipeline_result = 'Pipeline has failed.'
-else:
-    pipeline_result = 'Pipeline in unknown state.'
-
-
-import google.cloud.storage as gcs
-import os
-
-with open('pipeline_status.txt', 'w') as f:
-    f.write(pipeline_result)
-
-bucket.blob('easy_beam_test/pipeline_status.txt').upload_from_filename('pipeline_status.txt')
-if os.path.exists("pipeline_status.txt"):
-  os.remove("pipeline_status.txt")
+if __name__ == '__main__':
+  run()
