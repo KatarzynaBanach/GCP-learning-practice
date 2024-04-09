26aab25ccd894bd7ed6374eaeb11dedfa299108b
KatarzynaBanach <102869680+KatarzynaBanach@users.noreply.github.com>
2024-04-09 22:35:26 +0200

    Update init_settings.sh

--- Beam prod/init_settings.sh
+++ Beam prod/init_settings.sh
@@ -14,3 +14,7 @@ gcloud storage cp customer_data.csv gs://$BUCKET_NAME/customer/customer_data.csv
 # inform it there is a new source
 # podzielić na pliki, bilbioteki itp. osobno ta część z tworzeniem datasetu
 # w plikach można dać więcej statystyk odnośnie tych dzielonych tabel / spróbować połączyć je w jeden plik.
+
+
+# challanges:
+# difference between running locally and in dataflow eg. variables
